520804d81d829d3414fb9f4feabb038d1bbdf469
Jeffrey Burmood <mountainlifeissimple@icloud.com>
2024-07-17 06:29:41 -0600

    Misc updates

--- api/neo4j.py
+++ api/neo4j.py
@@ -9,10 +9,13 @@ Initiate the Neo4j Driver
 """
 # tag::initDriver[]
 def init_driver(uri, username, password):
-    # TODO: Create an instance of the driver here
-    current_app.driver = None
+    # create an instance of the driver here
+    current_app.driver = GraphDatabase.driver(uri, auth=(username, password))
 
-    return None
+    # verify connectivity
+    current_app.driver.verify_connectivity()
+
+    return current_app.driver
 # end::initDriver[]
 
 
